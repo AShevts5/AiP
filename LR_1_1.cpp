0ba44b50230292a57d6000d18f531541316d2166
Anna_Shevtsova9775 <annasevcova01@gmail.com>
2026-02-04 01:23:29 +0300

    Update LR_1_1.cpp

--- LR_1_1.cpp
+++ LR_1_1.cpp
@@ -3,30 +3,37 @@
 using namespace std;
 
 int main() {
-    int i = -20;
+    cout << "Натуральные числа в диапазоне от -20 до 100:" << endl;
+    
+    int j = -20; 
+    
+    do {
+        if (j > 0) {
+            if (j != 100)
+                cout << j << ", ";
+            else
+                cout << j;
+        }
+        j++;
+    } while (j <= 100);
+    
+    cout << endl;
+    
     long long p = 1;
     bool overflow = false;
     
-    cout << "Натуральные числа в диапазоне от -20 до 100:" << endl;
+    int i = -20; 
     
     do {
         if (i > 0) {
-            // Проверка на переполнение перед умножением
             if (p > LLONG_MAX / i) {
                 overflow = true;
-                break; // Прерываем цикл при переполнении
+                break;
             }
             p *= i;
-            
-            if (i != 100)
-                cout << i << ", ";
-            else
-                cout << i;
         }
         i++;
     } while (i <= 100);
-    
-    cout << endl;
     
     if (overflow) {
         cout << "Произведение слишком велико для типа long long!" << endl;
